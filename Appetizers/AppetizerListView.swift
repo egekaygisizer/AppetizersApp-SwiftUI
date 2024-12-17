53d9c4be4d0b25472c616400cf97ebb1645ef30e
Ege Kaygisizer <ekaygisizer@gmail.com>
2024-12-17 10:44:58 +0300

    Network system is okey, will it connect with UI now

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State var appetizers : [Appetizer] = []
+
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+            .onAppear { getAppetizers() }
         }
-        
     }
+    
+    func getAppetizers() {
+           NetworkManager.shared.fetchAppetizers { result in
+               DispatchQueue.main.async {
+                   switch result {
+                   case .success(let fetchedAppetizers):
+                       appetizers = fetchedAppetizers
+                   case .failure(_):
+                       print("Veri Yüklenmedi")
+                   }
+               }
+           }
+       }
 }
 
 #Preview {
     AppetizerListView()
 }
+
---
+++ Appetizers/NetworkManager.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkManager.swift
+//  Appetizers
+//
+//  Created by Ege Kaygisizer on 17/12/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let appetizerURL = baseURL + "appetizers"
+
+    
+    private init() {}
+    
+    func fetchAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url : url)) { data, response, error in
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let appetizers = try decoder.decode([Appetizer].self, from: data) // Veri bu değişkende saklı.
+                completed(.success(appetizers))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+    
+}
+
